4187cf92041265a8fe5d504c555ed886fece8c0f
FadlanBM <104897512+FadlanBM@users.noreply.github.com>
2023-07-03 06:35:32 +0700

    Update api.php

--- Web/routes/api.php
+++ Web/routes/api.php
@@ -14,4 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth:sanctum')->get('/userkuh', function (Request $request) {
+    return $request->user();
+});
